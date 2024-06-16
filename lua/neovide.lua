aac3ed48cc63df2f851a316d4bc6b0b745e7e4fd
Nadia Holmquist Pedersen <nadia@nhp.sh>
2024-06-16 23:01:10 +0200

    set remember window size anyway
    I actually just want it to start with a specific size but there's no
    option for that?

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,7 +2,7 @@ vim.o.guifont = "Fira Code:h15"
 
 vim.g.neovide_show_border = true
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_remember_window_size = false
+vim.g.neovide_remember_window_size = true
 
 vim.g.neovide_padding_top = 4
 vim.g.neovide_padding_bottom = 4
